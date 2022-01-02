52529eb84fc459a085d5c33ad57b4c8df706852e
Marian Milian <marian_milian@epam.com>
2022-01-02 11:55:39 +0200

    init

---
+++ src/main/java/com/milian/gof/patterns/GofPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.milian.gof.patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GofPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GofPatternsApplication.class, args);
+	}
+
+}
